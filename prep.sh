30a92157531ae24221dccb4bbcac1a21295bbf5a
Yfiua <yfiua6@gmail.com>
2024-07-29 15:12:32 +0200

    preparation code for downloading wiki-talk data

--- prep.sh
+++ prep.sh
@@ -3,3 +3,7 @@
 mkdir -p data
 wget https://linqs-data.soe.ucsc.edu/public/lbc/cora.tgz -O data/cora.tgz
 tar -xvzf data/cora.tgz -C data/
+
+wget https://zenodo.org/record/49561/files/wiki-talk-dataset.tar.gz -O data/wiki-talk-dataset.tar.gz
+tar -xvzf data/wiki-talk-dataset.tar.gz data/en-wiki-talk
+tar -xvzf data/wiki-talk-dataset.tar.gz data/en-user-group
