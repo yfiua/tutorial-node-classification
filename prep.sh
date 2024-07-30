8eb507fd75a309485593e0e057695de2c7c2720f
Yfiua <yfiua6@gmail.com>
2024-07-30 10:51:37 +0200

    role analysis with nx

--- prep.sh
+++ prep.sh
@@ -4,6 +4,7 @@ mkdir -p data
 wget https://linqs-data.soe.ucsc.edu/public/lbc/cora.tgz -O data/cora.tgz
 tar -xvzf data/cora.tgz -C data/
 
+# wiki-talk datasets
 wget https://zenodo.org/record/49561/files/wiki-talk-dataset.tar.gz -O data/wiki-talk-dataset.tar.gz
-tar -xvzf data/wiki-talk-dataset.tar.gz data/en-wiki-talk
-tar -xvzf data/wiki-talk-dataset.tar.gz data/en-user-group
+tar -xvzf data/wiki-talk-dataset.tar.gz -C data/
+mv data/data/ data/wiki-talk
