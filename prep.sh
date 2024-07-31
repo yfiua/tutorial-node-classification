868e93a22c75abe948f629ec2c6b117fcd65c525
Yfiua <yfiua6@gmail.com>
2024-07-31 18:00:16 +0200

    update tutorial

--- prep.sh
+++ prep.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 # This script is used to prepare the data for the training and testing of the model
 mkdir -p data
+
+# cora dataset
 wget https://linqs-data.soe.ucsc.edu/public/lbc/cora.tgz -O data/cora.tgz
 tar -xvzf data/cora.tgz -C data/
 
-# wiki-talk datasets
-wget https://zenodo.org/record/49561/files/wiki-talk-dataset.tar.gz -O data/wiki-talk-dataset.tar.gz
-tar -xvzf data/wiki-talk-dataset.tar.gz -C data/
-mv data/data/ data/wiki-talk
+# airports dataset
+mkdir -p data/airports
+
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/usa-airports.edgelist -P data/airports
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/brazil-airports.edgelist -P data/airports
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/europe-airports.edgelist -P data/airports
+
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/labels-usa-airports.txt -P data/airports
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/labels-brazil-airports.txt -P data/airports
+wget https://raw.githubusercontent.com/leoribeiro/struc2vec/master/graph/labels-europe-airports.txt -P data/airports
+
